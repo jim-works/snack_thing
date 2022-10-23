f93e6ef7226cb7fd5c211ea0cb9e5517a2a4f562
jim-works <jim4m80@gmail.com>
2022-10-23 14:31:06 -0500

    basic navigation of freight robot

---
+++ README.txt
@@ -0,0 +1,9 @@
+To run
+
+- `catkin_make`
+- `. devel/setup.bash`
+- `roslaunch snack_thing gazebo.launch`
+- wait for gazebo to open completely
+- `roslaunch snack_thing move.launch`
+
+Our code is in the `snack_thing` package (src/snack_thing).
---
+++ src/snack_controller.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import copy
+import actionlib
+import rospy
+
+from math import sin, cos
+from moveit_python import (MoveGroupInterface,
+                           PlanningSceneInterface,
+                           PickPlaceInterface)
+from moveit_python.geometry import rotate_pose_msg_by_euler_angles
+
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+from control_msgs.msg import PointHeadAction, PointHeadGoal
+from grasping_msgs.msg import FindGraspableObjectsAction, FindGraspableObjectsGoal
+from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from moveit_msgs.msg import PlaceLocation, MoveItErrorCodes
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+# Move base using navigation stack
+class MoveBaseClient(object):
+
+    def __init__(self):
+        self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        rospy.loginfo("Waiting for move_base...")
+        self.client.wait_for_server()
+
+    def goto(self, x, y, theta, frame="map"):
+        move_goal = MoveBaseGoal()
+        move_goal.target_pose.pose.position.x = x
+        move_goal.target_pose.pose.position.y = y
+        move_goal.target_pose.pose.orientation.z = sin(theta/2.0)
+        move_goal.target_pose.pose.orientation.w = cos(theta/2.0)
+        move_goal.target_pose.header.frame_id = frame
+        move_goal.target_pose.header.stamp = rospy.Time.now()
+
+        # TODO wait for things to work
+        self.client.send_goal(move_goal)
+        self.client.wait_for_result()
+
+if __name__ == "__main__":
+    # Create a node
+    rospy.init_node("snack_controller")
+
+    # Make sure sim time is working
+    while not rospy.Time.now():
+        pass
+
+    # Setup clients
+    move_base = MoveBaseClient()
+
+    # Move the base to be in front of the table
+    # Demonstrates the use of the navigation stack
+    rospy.loginfo("Moving to table...")
+    move_base.goto(2.250, 3.118, 0.0)
+
+    # Move to second table
+    rospy.loginfo("Moving to second table...")
+    move_base.goto(-3.53, 3.75, 1.57)
