3c6fd563d731096216a690f7bb2a9029353bee5b
hansikasundaresan <55006307+hansikasundaresan@users.noreply.github.com>
2022-10-23 19:45:04 -0500

    threshold updates

--- src/snack_controller.py
+++ src/snack_controller.py
@@ -17,16 +17,21 @@ from geometry_msgs.msg import PoseStamped
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from moveit_msgs.msg import PlaceLocation, MoveItErrorCodes
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from nav_msgs.msg import Odometry
 
+global_move_base = None
 # Move base using navigation stack
 class MoveBaseClient(object):
-
+    destination = []
+    current_pos = None
+    moving = False
     def __init__(self):
         self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         rospy.loginfo("Waiting for move_base...")
         self.client.wait_for_server()
 
     def goto(self, x, y, theta, frame="map"):
+        self.destination = [x,y,theta]
         move_goal = MoveBaseGoal()
         move_goal.target_pose.pose.position.x = x
         move_goal.target_pose.pose.position.y = y
@@ -37,7 +42,19 @@ class MoveBaseClient(object):
 
         # TODO wait for things to work
         self.client.send_goal(move_goal)
+        self.oving = True
         self.client.wait_for_result()
+
+    def cancel_goals(self):
+        distance = [999999,9999999,9999999]
+        self.client.cancel_all_goals()
+        self.moving = False
+
+    def distanceCalc(self,msg):
+        if self.moving:
+            current_pos = msg.pose.pose.position
+            if(abs(self.destination[0] - msg.pose.pose.position.x) < 0.1) or (abs(self.destination[1] - msg.pose.pose.position.y) < 0.1):
+                self.cancel_goals()
 
 if __name__ == "__main__":
     # Create a node
@@ -49,12 +66,17 @@ if __name__ == "__main__":
 
     # Setup clients
     move_base = MoveBaseClient()
-
+    global_move_base = move_base
     # Move the base to be in front of the table
     # Demonstrates the use of the navigation stack
     rospy.loginfo("Moving to table...")
+    rospy.Subscriber('odom',Odometry,move_base.distanceCalc)
     move_base.goto(2.250, 3.118, 0.0)
 
     # Move to second table
     rospy.loginfo("Moving to second table...")
     move_base.goto(-3.53, 3.75, 1.57)
+   
+
+    rospy.loginfo("comp..")
+
