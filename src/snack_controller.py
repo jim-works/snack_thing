b8af54310a3d4e08cf151d715972a9f9a5523088
jim-works <jim4m80@gmail.com>
2022-12-04 17:25:10 -0600

    refactor to send snack dispensers instead of coordinates, fix bugs with snack dispensing and multiple runs in a row

--- src/snack_controller.py
+++ src/snack_controller.py
@@ -30,8 +30,8 @@ class SnackBowl(object):
         self.y = 0
 
     def add_snack(self, volume):
-        rospy.loginfo(f"Added {volume} snack volume!")
         self.current_volume += volume
+        rospy.loginfo(f"Added {volume} snack volume! Current capacity: {self.current_volume}/{self.max_volume}.")
         if self.current_volume > self.max_volume:  # overflow!
             self.wasted += self.current_volume - self.max_volume
             self.current_volume = self.max_volume
@@ -51,58 +51,54 @@ class SnackDispenser:
         self.rate = rate
         self.check_interval = 0.05
         self.dispense_time_elapsed = 0
-        self.max_dispense_vol = snack_bowl.max_volume
-        self.start_dispensing()
         self.dispensing = False
         self.x = x
         self.y = y
         self.radius = radius
-        # check often if we are under a snack dispenser
-        rospy.Timer(rospy.Duration(self.check_interval), self.try_dispense)
+        rospy.Timer(rospy.Duration(self.check_interval), self.dispense)
 
-    def robot_under(self):
-        return (self.x-self.snack_bowl.x)**2 + (self.y-self.snack_bowl.y)**2 <= self.radius**2
+    def empty(self):
+        self.dispense_time_elapsed = 0
+        self.snack_bowl.current_volume = 0
+        rospy.loginfo("Emptied the snack bowl")
 
     def start_dispensing(self):
-        self.dispense_time_elapsed = 0
-        self.max_dispense_vol = self.snack_bowl.max_volume-self.snack_bowl.current_volume
+        if self.dispensing:
+            return #don't start again until we have stopped
         self.dispensing = True
+        self.dispense_time_elapsed = 0
+        rospy.loginfo("Start dispensing")
 
     def stop_dispensing(self):
+        if self.dispensing:
+            rospy.loginfo(f"Stop dispensing")
         self.dispensing = False
 
-    def update_dispensing(self):
-        if self.robot_under() and self.dispense_time_elapsed >= self.max_dispense_vol/self.rate:
-            self.stop_dispensing()
 
-    def try_dispense(self, event):
-        if not self.dispensing:
-            # try to start
-            if self.robot_under():
-                self.start_dispensing()
-        self.dispense_time_elapsed += self.check_interval
-        self.update_dispensing()
+    def dispense(self, event):
         if self.dispensing:
+            self.dispense_time_elapsed += self.check_interval
             self.drop_snack(self.rate*self.check_interval)
 
     def drop_snack(self, volume):
-        rospy.Timer(rospy.Duration(self.delay),
-                    lambda event: self.snack_bowl.add_snack(volume), oneshot=True)
+        rospy.Timer(rospy.Duration(self.delay), lambda event: self.snack_bowl.add_snack(volume), oneshot=True)
 
-# Move base using navigation stack
-
-
+        # Move base using navigation stack
 class MoveBaseClient(object):
     destination = []
     current_pos = None
     moving = False
 
-    def __init__(self, snack_bowl: SnackBowl):
+    def __init__(self, snack_bowl: SnackBowl, snack_dispensers):
         self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         rospy.loginfo("Waiting for move_base...")
         self.moving = False
         self.odom = None
         self.snack_bowl = snack_bowl
+        self.snack_dispensers = snack_dispensers
+        self.target_dispenser = None
+        self.check_interval = 0.05
+        rospy.Timer(rospy.Duration(self.check_interval), self.update_dispensing)
         self.client.wait_for_server()
 
     def goto(self, x, y, theta, frame="map"):
@@ -115,13 +111,37 @@ class MoveBaseClient(object):
         move_goal.target_pose.header.frame_id = frame
         move_goal.target_pose.header.stamp = rospy.Time.now()
 
-        # TODO wait for things to work
+        # wait for things to work
         self.client.send_goal(move_goal)
         self.moving = True
         self.client.wait_for_result()
 
-    def under_snack_dispenser(self):
-        return False
+    def under_target_dispenser(self):
+        if self.target_dispenser == None or self.target_dispenser < 0:
+            return False
+        disp = self.snack_dispensers[self.target_dispenser]
+        return (self.odom.pose.pose.position.x-disp.x)**2 + (self.odom.pose.pose.position.y-disp.y)**2 <= disp.radius**2
+
+    def update_dispensing(self, event):
+        if self.target_dispenser == None:
+            return
+        under_target = self.under_target_dispenser()
+        has_capacity = self.snack_dispensers[self.target_dispenser].dispense_time_elapsed < self.snack_bowl.max_volume/self.snack_dispensers[self.target_dispenser].rate
+        if under_target and has_capacity:
+            self.snack_dispensers[self.target_dispenser].start_dispensing()
+        else:
+            self.snack_dispensers[self.target_dispenser].stop_dispensing()
+
+    def wait_for_full(self):
+        while self.under_target_dispenser() and not self.snack_bowl.full():
+            rospy.loginfo(f"Robot is waiting to be filled from dispenser {self.target_dispenser}. Current capacity: {self.snack_bowl.current_volume}")
+            rospy.sleep(0.2)
+            
+
+    def clear_target(self):
+        self.snack_dispensers[self.target_dispenser].stop_dispensing()
+        self.snack_dispensers[self.target_dispenser].dispense_time_elapsed = 0
+        self.target_dispenser = None
 
     def cancel_goals(self):
         rospy.loginfo("cancelling!!!!")
@@ -138,8 +158,7 @@ class MoveBaseClient(object):
             #rospy.loginfo(f"odom: ({self.odom.pose.pose.position.x},{self.odom.pose.pose.position.y}) x: {abs(self.destination[0] - self.odom.pose.pose.position.x)}, y: {abs(self.destination[1] - self.odom.pose.pose.position.y)}")
             dist = (self.destination[0] - self.odom.pose.pose.position.x)**2 + (
                 self.destination[1] - self.odom.pose.pose.position.y)**2
-            rospy.loginfo(
-                f"odom: {self.odom.pose.pose.position}, dest: {self.destination}, dist: {dist}")
+            #rospy.loginfo(f"odom: {self.odom.pose.pose.position}, dest: {self.destination}, dist: {dist}")
             if dist < 1:
                 self.cancel_goals()
 
@@ -153,19 +172,19 @@ if __name__ == "__main__":
         pass
     snack_bowl = SnackBowl()
     snack_bowl.max_volume = 1
-    pretzels = SnackDispenser(snack_bowl, 0.5, 0.5, 3.05, -4, 2)
-    goldfish = SnackDispenser(snack_bowl, 0.5, 0.5, 3.05, 0, 2)
-    cheez_its = SnackDispenser(snack_bowl, 0.5, 0.5, 3.05, 4, 2)
+    pretzels = SnackDispenser(snack_bowl, 0.5, 0.5, 3.05, -4, 1.5)
+    goldfish = SnackDispenser(snack_bowl, 0.5, 0.5, 3.05, 0, 1.5)
+    cheez_its = SnackDispenser(snack_bowl, 0.5, 0.5, 3.05, 4, 1.5)
 
     HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-    PORT = 65423  # Port to listen on (non-privileged ports are > 1023)
+    PORT = 65422  # Port to listen on (non-privileged ports are > 1023)
 
-    move_base = MoveBaseClient(snack_bowl)
+    move_base = MoveBaseClient(snack_bowl, [pretzels, goldfish, cheez_its])
     rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, move_base.get_odom)
     rospy.Timer(rospy.Duration(0.1), move_base.distanceCalc)
 
     # read coordinates from network
-    """with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         print(f"Ready on {HOST}:{PORT}")
@@ -181,31 +200,25 @@ if __name__ == "__main__":
                 if not data:
                     break
                 try:
-                    coords = data.decode('utf-8')
-                    x = float(coords.split(',')[0])
-                    y = float(coords.split(',')[1])
-                    print(f"moving to {x}, {y}")
+                    command = data.decode('utf-8') #<dispenser_id>,<x>,<y>
+                    rospy.loginfo(command)
+                    dispenser_id = int(command.split(',')[0])
+                    rospy.loginfo(f"id: {dispenser_id}")
+                    x = float(command.split(',')[1])
+                    y = float(command.split(',')[2])
+                    rospy.loginfo(f"Moving to dispenser {dispenser_id}")
+                    move_base.snack_bowl.current_volume = 0 #empty the snack bowl before going, yum!
+                    move_base.snack_dispensers[dispenser_id].stop_dispensing()
+                    move_base.target_dispenser = dispenser_id
+                    move_base.goto(move_base.snack_dispensers[dispenser_id].x, move_base.snack_dispensers[dispenser_id].y, 0)
+                    rospy.loginfo(f"moving to {x}, {y}")
+                    move_base.wait_for_full()
+                    move_base.clear_target()
                     move_base.goto(x, y, 0)
-                    print("Done!")
+                    rospy.loginfo("Done!")
                 except Exception as e:
-                    print("Error occurred while trying to move: " + str(e))
+                    rospy.loginfo("Error occurred while trying to move: " + str(e))
     """
+    move_base.goto(0,0,0)
     move_base.goto(3.05,-4,0)
 """
-    # Setup clients
-    move_base = MoveBaseClient()
-    # Move the base to be in front of the table
-    # Demonstrates the use of the navigation stack
-    rospy.loginfo("Moving to snack dispenser...")
-    rospy.Subscriber('odom',Odometry,move_base.get_odom)
-    rospy.Timer(rospy.Duration(0.1), move_base.distanceCalc)
-    #move_base.goto(4.03, -3, 0.0)
-    move_base.goto(2.250, 3.118, 0.0)
-
-    # Move to second table
-    rospy.loginfo("Moving to table...")
-    #move_base.goto(-3.53, 3.75, 0.0)
-   
-
-    rospy.loginfo("comp..")
-    """
--- src/streamlit_file.py
+++ src/streamlit_file.py
@@ -4,29 +4,24 @@ from PIL import Image
 import streamlit as st
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65423  # The port used by the server
+PORT = 65422  # The port used by the server
 
 
-def coor_for_robot(x, y):
+def send_command(dispenser_id, x, y):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         st.write("Moving!")
-        s.sendall(bytes(f"{x},{y}", "utf-8"))
+        s.sendall(bytes(f"{dispenser_id},{x},{y}", "utf-8"))
         data = s.recv(1024)
         st.write("Done!")
 
-
 def submit_coordinates(x, y, option):
-    coord = ()
-    if (option == "Pretzels"):
-        coord = (3.05, -4)
-    elif (option == "Gold Fish"):
-        coord = (3.05, 0)
-    else:
-        coord = (3.05, 4)
-    st.write(coord)
-    coor_for_robot(coord[0], coord[1])
-    coor_for_robot(x, y)
+    dispensers = {
+        "Pretzels": 0,
+        "Gold Fish": 1,
+        "Cheese Its": 2
+    }
+    send_command(dispensers[option], x, y)
     return
 
 
